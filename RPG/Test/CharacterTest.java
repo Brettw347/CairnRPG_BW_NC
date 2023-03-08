138bb0df44774e6ee66ecb497bb70bdd4ceb6d34
Brettw347 <brett.williams.347@gmail.com>
2023-03-08 16:06:17 -0500

    Update CharacterTest.java

--- RPG/Test/CharacterTest.java
+++ RPG/Test/CharacterTest.java
@@ -9,51 +9,51 @@ class CharacterTest {
     @Test
     @DisplayName("Normal Fight")
     void FightTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
-        Enemy enemy = new Character ("Enemy", 100,10, true).new Enemy(30);
-        assertEqual(true, hero.Fight(enemy));
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Enemy enemy = new Enemy("Enemy", 100, 30, true, 30);
+        assertEquals(true, hero.Fight(enemy));
     }
 
     @Test
     @DisplayName("Normal useItem")
     void useItemTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0, new String[]{"Potion"});
-        Enemy enemy = new Character ("Enemy", 100,10, true).new Enemy(30);
-        assertEquals(0, useItem(0));
-
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Enemy enemy = new Enemy("Enemy", 100, 30, true, 30);
+        assertEquals(0, hero.useItem(0));
     }
 
     @Test
     @DisplayName("Hero has enough experience to level up")
     void levelUpTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 100, 0);
-        assertEqual(true, levelUp());
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        assertEquals(true, hero.levelUp());
     }
 
     @Test
     @DisplayName("Hero does not have enough experience to level up")
     void levelUpTest2(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
-        assertEquals(false, levelUp());
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        assertEquals(false, hero.levelUp());
     }
 
     @Test
     @DisplayName("Normal addToInventory")
     void addToInventoryTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
-        assertEquals(true, addToInventory(0));
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        assertEquals(true, hero.addToInventory("Potion"));
     }
 
     @Test
     @DisplayName("Normal showInventory")
     void showInventoryTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0,new String[]{"Potion", "Potion"});
-        assertEquals("Potion, Potion", showInventory());
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        assertEquals("Potion, Potion", hero.showInventory());
     }
+
     @Test
     @DisplayName("Display Hero Info")
     void InfoTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         assertEquals("Hero, 100, 10, true", hero.Info());
     }
 
@@ -61,14 +61,14 @@ class CharacterTest {
     @Test
     @DisplayName("Normal Enemy Fight")
     void enemyFightTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
-        Enemy enemy = new Character ("Enemy", 100,10, true).new Enemy(30);
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Enemy enemy = new Enemy("Enemy", 100, 30, true, 30);
         assertEquals(true, enemy.Fight(hero));
     }
     @Test
     @DisplayName("Display Enemy Info")
     void enemyInfoTest1(){
-        Enemy enemy = new Character ("Enemy", 100,10, true).new Enemy(30);
+        Enemy enemy = new Enemy("Enemy", 100, 30, true, 30);
         assertEquals("Enemy, 100, 10, 30, true", enemy.Info());
     }
 
@@ -76,15 +76,15 @@ class CharacterTest {
     @Test
     @DisplayName("Normal Boss Fight")
     void bossFightTest1(){
-        Hero hero = new Character("Hero", 100, 10, true).new Hero(1, 0, 0);
-        Boss boss = new Character ("Boss", 100,10, true).new Enemy(30).new Boss(3);
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Boss boss = new Boss("Final Boss", 100, 10, true, 30,3);
         assertEquals(true, boss.Fight(hero));
     }
 
     @Test
     @DisplayName("Display Boss Info")
     void bossInfoTest1(){
-        Boss boss = new Character ("Boss", 100,10, true).new Enemy(30).new Boss(3);
+        Boss boss = new Boss("Final Boss", 100, 10, true, 30,3);
         assertEquals("Boss, 100, 10, 30, 3, true", boss.Info());
     }
 }
