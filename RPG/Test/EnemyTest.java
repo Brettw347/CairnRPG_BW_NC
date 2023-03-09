8772594376c215066be4b3cb080aea1298e9cbc3
Brettw347 <brett.williams.347@gmail.com>
2023-03-08 22:08:38 -0500

    Added unit tests to the Enemy and Boss classes

---
+++ RPG/Test/EnemyTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    @DisplayName("Normal fight method")
+    void fightTest1() {
+        Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Enemy enemy = new Enemy("Enemy", 100, 10,true,30);
+        assertEquals(true, enemy.Fight(hero));
+    }
+
+    @Test
+    @DisplayName("fight method with hero taking damage that would defeat him")
+    void fightTest2() {
+        Hero hero = new Hero("Hero", 10, 10, true, 1, 0,0.0, new String[]{"Potion"});
+        Enemy enemy = new Enemy("Enemy", 100, 10,true,30);
+        assertEquals(true, enemy.Fight(hero));
+    }
+    @Test
+    @DisplayName("Normal info method")
+    void testInfo1() {
+    }
+
+}
--- RPG/Test/HeroTest.java
+++ RPG/Test/HeroTest.java
@@ -1,90 +1,93 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
 
-    //Normal fight method
     @Test
+    @DisplayName("Normal Fight Method")
     void fightTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         Enemy enemy = new Enemy("Enemy", 100, 10,true,30);
         assertEquals(true, hero.Fight(enemy));
     }
-    //fight method with enemy taking damage that would defeat them
+
     @Test
+    @DisplayName("fight method with enemy taking damage that would defeat it")
     void fightTest2() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         Enemy enemy = new Enemy("Enemy", 10, 10,true,30);
         assertEquals(true, hero.Fight(enemy));
     }
 
-    //Normal useItem method
     @Test
+    @DisplayName("Normal useItem method")
     void useItemTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         Enemy enemy = new Enemy("Enemy", 100, 10,true,30);
         assertEquals(true, hero.useItem(0));
     }
 
-    //useItem with no items in inventory
     @Test
+    @DisplayName("useItem with no items in inventory")
     void useItemTest2() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{null});
         Enemy enemy = new Enemy("Enemy", 100, 10,true,30);
         assertEquals(false, hero.useItem(0));
     }
 
-    //Normal levelUp method
     @Test
+    @DisplayName("Normal levelUp method")
     void levelUpTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 100,0.0, new String[]{"Potion"});
         assertEquals(true, hero.levelUp());
     }
 
-    //levelUp with not enough experience to level up
     @Test
+    @DisplayName("levelUp with not enouogh experience to level up")
     void levelUpTest2() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         assertEquals(false, hero.levelUp());
     }
 
-    //levelUp with higher level, health, and attack power
     @Test
+    @DisplayName("levelUp with higher leve, health, and attack power values")
     void levelUpTest3() {
         Hero hero = new Hero("Hero", 110, 11, true, 2, 100,0.0, new String[]{"Potion"});
         assertEquals(true, hero.levelUp());
     }
 
-    //addToInventory with 1 item in the inventory
     @Test
+    @DisplayName("addToInventory with 1 item in the inventory")
     void addToInventoryTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion", null});
         assertEquals(true, hero.addToInventory("Potion"));
     }
 
-    //addToInventory with full inventory
     @Test
+    @DisplayName("addToInvetory with full inventory")
     void addToInventoryTest2() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion", "Potion", "Potion", "Potion", "Potion", "Potion"});
         assertEquals(false, hero.addToInventory("Potion"));
     }
 
-    //Normal addToInventory, adding to an empty inventory
     @Test
+    @DisplayName("Normal addToInventory method; adding to an empty inventory")
     void addToInventoryTest3() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{null});
         assertEquals(true, hero.addToInventory("Potion"));
     }
 
-    //Not Working
     @Test
+    @DisplayName("Normal showInventory method; Not Working")
     void showInventoryTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         assertEquals("Inventory:\n" + "Potion" + "\n", hero.showInventory());
     }
 
     @Test
+    @DisplayName("Normal info method")
     void infoTest1() {
         Hero hero = new Hero("Hero", 100, 10, true, 1, 0,0.0, new String[]{"Potion"});
         assertEquals(true, hero.info());
